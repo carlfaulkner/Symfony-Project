33c8480c01dab73d5029622eb81a186367bd112d
Carl Faulkner <carlfaulkner@email.com>
2022-06-25 15:02:57 +0100

    Added twig templating

--- src/Controller/TasksController.php
+++ src/Controller/TasksController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TasksController
+class TasksController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Home');
+        return $this->render('homepage/homepage.html.twig', [
+            'title' => 'Home Page',
+        ]);
     }
 
     #[Route('/tasks')]
