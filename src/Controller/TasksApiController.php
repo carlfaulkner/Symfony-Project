b1c66e543109240217de916eb23e4f9aa2cde712
Carl Faulkner <carlfaulkner@email.com>
2022-06-25 16:42:27 +0100

    All good so far!!

--- src/Controller/TasksApiController.php
+++ src/Controller/TasksApiController.php
@@ -5,17 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
 
 class TasksApiController extends AbstractController
 {
     #[Route('/api/tasks/{id<\d+>}', methods: ['GET'])]
 
-    public function getTasks(int $id): Response
+    public function getTasks(int $id, LoggerInterface $logger): Response
     {
         $tasks = [
             'id' => $id,
             'title' => 'test title',
         ];
+
+        $logger->info('Returning API response for task {task}',[
+            'task' => $id,
+        ]);
 
         return $this->json($tasks);
     }
