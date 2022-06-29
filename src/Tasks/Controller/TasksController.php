88447ae5da87dd1a3a155e53fdf5869755802944
Carl Faulkner <carlfaulkner@email.com>
2022-06-29 18:39:49 +0100

    symfony forms added

--- src/Tasks/Controller/TasksController.php
+++ src/Tasks/Controller/TasksController.php
@@ -14,6 +14,7 @@ final class TasksController extends AbstractController
     public function tasks(): Response
     {
         $tasks = new TasksFacade();
+
         $tasks_response = $tasks->listTasks();
               
         $tasks = $tasks_response['data']['tasks'];
@@ -24,16 +25,25 @@ final class TasksController extends AbstractController
         ]);
     }
 
-    #[Route('/tasks/{task_id}', name: 'app_task')]
-    public function task($task_id): Response
+    #[Route('/tasks/{task_id<\d+>}', name: 'app_task')]
+    public function task(int $task_id): Response
     {
         $tasks = new TasksFacade();
+
         $tasks_response = $tasks->getTask($task_id);
 
         $task = $tasks_response['data']['tasks'][0];
 
-        return $this->render('task/task.html.twig', [
+        return $this->render('tasks/task.html.twig', [
             'title' => $task['title'],
+        ]);
+    }
+
+    #[Route('/tasks/add', name: 'app_task_add')]
+    public function addTask(): Response
+    {
+        return $this->render('tasks/task-add.html.twig', [
+            'title' => 'Add Task',
         ]);
     }
 }
